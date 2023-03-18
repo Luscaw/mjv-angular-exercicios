5d47d41a0f6d3462ed74e7f98da0df7290cde338
Lucas Emanoel <luscaemanoel@gmail.com>
2023-03-18 15:33:20 -0300

    Excluindo pasta de componentes e inserido pasta do segundo exercício

--- segundo-exercicio/src/app/app.component.spec.ts
+++ segundo-exercicio/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aula-componente'`, () => {
+  it(`should have as title 'segundo-exercicio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aula-componente');
+    expect(app.title).toEqual('segundo-exercicio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('aula-componente app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('segundo-exercicio app is running!');
   });
 });
--- segundo-exercicio/src/app/app.component.ts
+++ segundo-exercicio/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'aula-componente';
+  title = 'segundo-exercicio';
 }
